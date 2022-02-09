257a25a5a9ea872e1a37e6886480513c2545715d
Matheus Alves <matheusalvesrs20@gmail.com>
2022-02-09 12:09:53 -0300

    Ex78 melhorado e com def

--- Exercícios do Guanabara sendo refeitos em 2022/Revisaoguanabara/Ambiente_revisao.py
+++ Exercícios do Guanabara sendo refeitos em 2022/Revisaoguanabara/Ambiente_revisao.py
@@ -1,25 +1,29 @@
-#Exercício Python 080: Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort() No final, mostre a lista ordenada na tela.
-from random import randint
+#Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista. 
 
-def number_in_correct_position():
-    inserted_numbers = []
-    print("Original Values generated in the following sequence: ",end= '')
-    for c in range(0,5):
-        user_values = randint(1,1000)
-        if c != 4:
-            print(user_values,end= ',')
+
+
+
+
+
+def major_and_minor():
+    numbers = []
+    major_value = minor_value = 0
+    cont = 0
+    while cont < 5:
+        try:
+            numbers.append(int(input(f'Enter a valid {cont + 1}º number: ')))
+            cont += 1
+        except:
+            print("Invalid character! Try again.")
+            continue
+    for k, v in enumerate(numbers):
+        if k == 0:
+            major_value = minor_value = v
         else:
-            print(user_values,end='.')
-
-
-        if c == 0 or user_values >= inserted_numbers[-1]:
-            inserted_numbers.append(user_values)
-
-        else:
-            for key, value in enumerate(inserted_numbers):
-                if user_values <= value:
-                    inserted_numbers.insert(key,user_values)
-                    break
-    print()
-    print(inserted_numbers)
-number_in_correct_position()
+            if v > major_value:
+                major_value = v
+            if v < minor_value:
+                minor_value = v
+    return {"Major value": major_value, "Minor value":minor_value}
+answers = major_and_minor()
+print(answers)
--- Exercícios do Guanabara sendo refeitos em 2022/Revisaoguanabara/ex78.py
+++ Exercícios do Guanabara sendo refeitos em 2022/Revisaoguanabara/ex78.py
@@ -1,28 +1,27 @@
 #Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista. 
 
 
-#utilizando lógica
-maior = menor = 0
-valores = []
-for c in range(0,5):
-    valores.append(int(input(f'Digite o valor na posição {c}')))
-    if c == 0:
-        maior = menor = valores[c]
-    else:
-        if valores[c] > maior:
-            maior = valores[c]
-        if valores[c] < menor:
-            menor = valores[c]
-print(f'Você digitou {valores}')
+def major_and_minor():
+    numbers = []
+    major_value = minor_value = 0
+    cont = 0
+    while cont < 5:
+        try:
+            numbers.append(int(input(f'Enter a valid {cont + 1}º number: ')))
+            cont += 1
+        except:
+            print("Invalid character! Try again.")
+            continue
+    for k, v in enumerate(numbers):
+        if k == 0:
+            major_value = minor_value = v
+        else:
+            if v > major_value:
+                major_value = v
+            if v < minor_value:
+                minor_value = v
+    return {"Major value": major_value, "Minor value":minor_value}
 
-print(f'O maior valor foi {maior} na(s) posição(ões): ',end= '')
-for i, v in enumerate(valores):
-    if v == maior:
-        print(f'{i}...',end='')
-print()
-print(f'O menor valor foi {menor} na(s) posição(ões): ',end= '')
-for i, v in enumerate(valores):
-    if v == menor:
-        print(f'{i}...',end= '')
-
-print()
+    
+answers = major_and_minor()
+print(answers)
