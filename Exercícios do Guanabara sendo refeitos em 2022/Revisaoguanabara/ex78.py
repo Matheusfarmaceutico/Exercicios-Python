1d74e2edb0cc54c4e107e3fe26e1df08dd482ff8
Matheus Alves <matheusalvesrs20@gmail.com>
2022-02-11 16:34:41 -0300

    Ex 78 refeito

--- Exercícios do Guanabara sendo refeitos em 2022/Revisaoguanabara/ex78.py
+++ Exercícios do Guanabara sendo refeitos em 2022/Revisaoguanabara/ex78.py
@@ -1,27 +1,33 @@
 #Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista. 
 
 
-def major_and_minor():
-    numbers = []
-    major_value = minor_value = 0
+
+def major_minor_pos():
+    list_numbers = []
+    major = minor = 0
     cont = 0
     while cont < 5:
         try:
-            numbers.append(int(input(f'Enter a valid {cont + 1}º number: ')))
+            number = int(input(f'Enter a valid {cont + 1}:  '))
             cont += 1
         except:
-            print("Invalid character! Try again.")
+            print('Invalid. Try Again!')
             continue
-    for k, v in enumerate(numbers):
-        if k == 0:
-            major_value = minor_value = v
+        list_numbers.append(number)
+    for key, value in enumerate(list_numbers):
+        if key == 0:
+            major = minor = value
         else:
-            if v > major_value:
-                major_value = v
-            if v < minor_value:
-                minor_value = v
-    return {"Major value": major_value, "Minor value":minor_value}
+            if value > major:
+                major = value
+            if value < minor:
+                minor = value
+    for key, value in enumerate(list_numbers):
+        if value == major:
+            print(f'Major is {value}, whose is at position {key + 1}')
+        if value == minor:
+            print(f'Minor is {value}, whose is at position {key + 1} ')
+    return ''
 
-    
-answers = major_and_minor()
-print(answers)
+test = major_minor_pos()
+print(test)
