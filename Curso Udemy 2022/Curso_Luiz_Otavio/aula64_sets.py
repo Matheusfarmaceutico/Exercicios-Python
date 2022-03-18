6045fee88cefa5d41620cac9af1605c4d662a082
Matheus <matheusalvesrs20@gmail.com>
2022-03-18 01:59:16 -0300

    sets finalizados

--- Curso Udemy 2022/Curso_Luiz_Otavio/aula64_sets.py
+++ Curso Udemy 2022/Curso_Luiz_Otavio/aula64_sets.py
@@ -36,6 +36,7 @@ print(s3)
 s3 = s2 - s1
 print(s3)'''
 
-s1 = {1,2,3,4,5}
+'''s1 = {1,2,3,4,5}
 s2 = {1,2,3,4,6}
-s3 = s1 
+s3 = s2 ^ s1 # symmetric_difference = ^ vai mostrar elementos elementos incomuns nos dois sets
+print(s3)'''
