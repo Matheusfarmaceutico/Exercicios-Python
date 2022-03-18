0efccb9673bccf5b2aff09a926fd69817ed33887
Matheus <matheusalvesrs20@gmail.com>
2022-03-17 23:11:32 -0300

    ex63_finalizado

--- Curso Udemy 2022/Curso_Luiz_Otavio/ex63_dicionarios_perguntas_respostas.py
+++ Curso Udemy 2022/Curso_Luiz_Otavio/ex63_dicionarios_perguntas_respostas.py
@@ -8,17 +8,18 @@ perguntas = {
     'alternativas':{'a':'13','b':'14','c':'12'},
     'resposta_certa':'c'},
 }
+quant_acertos = 0
 for pk, pv in perguntas.items():
     print(f'{pk}: {pv["pergunta"]}')
     print('Alternativas: ')
     for rk, rv in pv['alternativas'].items():
         print(f'[{rk}]: {rv}')
 
-
+    
     usr_resp = input('Digite a sua resposta: ')
 
 
-    quant_acertos = 0
+    
     if usr_resp == pv['resposta_certa']:
         print('Resposta Correta!')
         quant_acertos += 1
@@ -26,7 +27,7 @@ for pk, pv in perguntas.items():
         print('Resposta Errada!')
     print()
 quant_perguntas = len(perguntas)
-print(quant_perguntas)
+print(f'Você acertou {quant_acertos}, ou seja, {quant_acertos / quant_perguntas * 100} % das questões ')
     
         
         
