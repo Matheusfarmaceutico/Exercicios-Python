5de725b69a04a7533aa22aa19c187257676f2eb2
Matheus <matheusalvesrs20@gmail.com>
2022-03-23 21:51:39 -0300

    Aula67_completa

---
+++ Curso Udemy 2022/Curso_Luiz_Otavio/aula67_exercicio.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+string = '012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789'
+
+def separador(args):
+    lista = [args[i: i + 10] for i in range(0,len(string), 10)]
+    return ".".join(lista)
+
+
+print(separador(string))
