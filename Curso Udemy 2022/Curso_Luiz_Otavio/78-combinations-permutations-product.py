43fea6c5481973e0e3929d59e8aa1c9b15cc5e06
Matheus Alves <matheusalvesrs20@gmail.com>
2022-04-07 18:31:43 -0300

    inicio-aula-79

--- Curso Udemy 2022/Curso_Luiz_Otavio/78-combinations-permutations-product.py
+++ Curso Udemy 2022/Curso_Luiz_Otavio/78-combinations-permutations-product.py
@@ -6,23 +6,24 @@ Permutação - Ordem importa
 Ambos não repetem valores únicos.
 Produto - Ordem importa e repete valores únicos
 """
-
+def separador():
+    return "-="*30
 #Combinations
 
 from itertools import combinations
 pessoas = ["Luiz","André","Eduardo","Letícia", "Fabrício","Rose"]
 for grupo in combinations(pessoas,2):
     print(grupo)
-"""A ordem não importa significa que se eu tiver Eduardo, Rose, não terei Rose,Educardo."""
-
-#permutacions
+"""A ordem não importa significa que se eu tiver Eduardo, Rose, não terei Rose,Eduardo."""
+print(separador())
+#permutations
 
 from itertools import permutations
 pessoas = ["Luiz","André","Eduardo","Letícia", "Fabrício","Rose"]
 for grupo in permutations(pessoas,2):
     print(grupo)
-"""Nesse caso de permutações pode acontecer Eduardo, Rose e Rose e Edurado"""
-
+"""Nesse caso de permutações pode acontecer Eduardo,Rose e Rose e Eduardo"""
+print(separador())
 # Product - Permitem que haja combinações entre o próprio elemento, como Luiz e Luiz
 
 from itertools import product
---
+++ Curso Udemy 2022/Curso_Luiz_Otavio/79_aula_groupby.py
@@ -0,0 +1,9 @@
+alunos = {
+    {"Nome":"Luiz","nota":"A"},
+    {"Nome":"Letícia","nota":"B"},
+    {"Nome":"Fabrício","nota":"A"},
+    {"Nome":"Rosemary","nota":"C"},
+    {"Nome":"Joana","nota":"D"},
+    {"Nome":"João", "nota":"A"},{"Nome":"Eduardo", "nota":"B"},{"Nome":"André", "nota":"A"},{"Nome":"Anderson", "nota":"C"},
+    {"Nome":"José", "nota":"B"}
+}
