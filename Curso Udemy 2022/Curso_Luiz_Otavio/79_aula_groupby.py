f8b9cdea3da7e4ac012dc57d24af7a9916f31ae6
Matheus Alves <matheusalvesrs20@gmail.com>
2022-04-07 21:19:37 -0300

    aula_79-finalizada

--- Curso Udemy 2022/Curso_Luiz_Otavio/79_aula_groupby.py
+++ Curso Udemy 2022/Curso_Luiz_Otavio/79_aula_groupby.py
@@ -53,6 +53,8 @@ print(separador())
 
 
 #agrupando, de fato, os grupos.
+
+# 1.0 - Verificando quantas pessoas tiram determinada Nota.
 alunos = [
     {"Nome":"Luiz","nota":"A"},
     {"Nome":"Letícia","nota":"B"},
@@ -63,12 +65,82 @@ alunos = [
     {"Nome":"José", "nota":"B"},
 ]
 
-ordena = lambda item: item["nota"]
-alunos.sort(key = ordena)
-agrupar = groupby(alunos,ordena)
+organizar = lambda item: item['nota']
+alunos.sort(key = organizar)
+agrupamento = groupby(alunos, organizar)
+for agrupamento, dados_person in agrupamento:
+   print(f'Agrupamento: {agrupamento}')
+   quantidade = len(list(dados_person))
+   print(quantidade)
+print(separador())
+# 2.0 - Verificando quais pessoas tiraram determinada nota.
+alunos = [
+    {"Nome":"Luiz","nota":"A"},
+    {"Nome":"Letícia","nota":"B"},
+    {"Nome":"Fabrício","nota":"A"},
+    {"Nome":"Rosemary","nota":"C"},
+    {"Nome":"Joana","nota":"D"},
+    {"Nome":"João", "nota":"A"},{"Nome":"Eduardo", "nota":"B"},{"Nome":"André", "nota":"A"},{"Nome":"Anderson", "nota":"C"},
+    {"Nome":"José", "nota":"B"},
+]
 
-for agrupamento, valores_agrupados in agrupar:
+organizar = lambda item: item['nota']
+alunos.sort(key = organizar)
+agrupamento = groupby(alunos, organizar)
+for agrupamento, dados_person in agrupamento:
+    print()
     print(f'Agrupamento: {agrupamento}')
-    valores_agrupados = list(valores_agrupados)
-    for pessoas in valores_agrupados:
-        print(pessoas)
+    print()
+    for agrupamento in dados_person:
+        print(agrupamento["Nome"])
+    
+print(separador())
+
+#3.0 - Mostrando quantidade pessoas que estao no meu grupo e o nome dessas pessoas.
+""" Note que esse vai ser um caso mais complexo, pois uma vez que eu esgoto meu interado..."""
+from itertools import groupby
+alunos = [
+    {"Nome":"Luiz","nota":"A"},
+    {"Nome":"Letícia","nota":"B"},
+    {"Nome":"Fabrício","nota":"A"},
+    {"Nome":"Rosemary","nota":"C"},
+    {"Nome":"Joana","nota":"D"},
+    {"Nome":"João", "nota":"A"},{"Nome":"Eduardo", "nota":"B"},{"Nome":"André", "nota":"A"},{"Nome":"Anderson", "nota":"C"},
+    {"Nome":"José", "nota":"B"},
+]
+
+organizar = lambda item: item['nota']
+alunos.sort(key = organizar)
+agrupamento = groupby(alunos, organizar)
+for agrupamento, dados_person in agrupamento:
+    print()
+    print(f'Agrupamento: {agrupamento}')
+    print()
+    quantidade = len(list(dados_person))
+    print(quantidade)
+    for agrupamento in dados_person:
+        print(agrupamento["Nome"])
+    #Note que eu fizer dessa forma, nao consigo mostrar o nome dos alunos, pois os dados de iteradores já foi consumido, havendo a necessidade de criar uma cópia de dados_person, através do módulo tee.
+print(separador())
+from itertools import groupby,tee
+alunos = [
+    {"Nome":"Luiz","nota":"A"},
+    {"Nome":"Letícia","nota":"B"},
+    {"Nome":"Fabrício","nota":"A"},
+    {"Nome":"Rosemary","nota":"C"},
+    {"Nome":"Joana","nota":"D"},
+    {"Nome":"João", "nota":"A"},{"Nome":"Eduardo", "nota":"B"},{"Nome":"André", "nota":"A"},{"Nome":"Anderson", "nota":"C"},
+    {"Nome":"José", "nota":"B"},
+]
+organizar = lambda item: item["nota"]
+agrupar = groupby(alunos,organizar)
+alunos.sort(key = organizar)
+grupos = groupby(alunos,organizar)
+for agrupamento, full_dados in grupos:
+    var1, var2 = tee(full_dados)
+    print()
+    print(f"Agrupamento:  {agrupamento}")
+    for aluno in var1:
+        print(aluno["Nome"])
+    quantidade = len(list(var2))
+    print(f'A quantidade de alunos que tirou a nota {agrupamento} foi {quantidade}')
