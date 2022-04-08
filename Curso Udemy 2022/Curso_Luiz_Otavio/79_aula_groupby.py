bb75296d7e4ea2b6ccfca0bc7a39bb204b6ba40d
Matheus Alves <matheusalvesrs20@gmail.com>
2022-04-07 21:40:03 -0300

    gropby-finalizado-com-explicacao

--- Curso Udemy 2022/Curso_Luiz_Otavio/79_aula_groupby.py
+++ Curso Udemy 2022/Curso_Luiz_Otavio/79_aula_groupby.py
@@ -132,15 +132,18 @@ alunos = [
     {"Nome":"João", "nota":"A"},{"Nome":"Eduardo", "nota":"B"},{"Nome":"André", "nota":"A"},{"Nome":"Anderson", "nota":"C"},
     {"Nome":"José", "nota":"B"},
 ]
-organizar = lambda item: item["nota"]
-agrupar = groupby(alunos,organizar)
-alunos.sort(key = organizar)
-grupos = groupby(alunos,organizar)
-for agrupamento, full_dados in grupos:
-    var1, var2 = tee(full_dados)
+
+organizar = lambda iter: iter["nota"]
+alunos_ordenados = sorted(alunos, key = organizar)
+grupos_separados = groupby(alunos_ordenados,organizar)
+
+for agrupamentos, full_dados in grupos_separados:
     print()
-    print(f"Agrupamento:  {agrupamento}")
-    for aluno in var1:
-        print(aluno["Nome"])
-    quantidade = len(list(var2))
-    print(f'A quantidade de alunos que tirou a nota {agrupamento} foi {quantidade}')
+    print(f"Agrupamentos {agrupamentos}")
+    v1, v2 = tee(full_dados) #trata-se de um iterador que se esgota, por isso tee faz duas cópias de "full dados", para que eu consiga exibir os nomes dos alunos e a quantidade deles que tiraram uma determinada nota"
+
+    for aluno in v1:
+        print(aluno["Nome"]) #V1 é uma cópia de full_dados, e a utilizo aqui para que eu possa exibir o nome do aluno daquele grupamento em específico.
+    quantidade = len(list(v2)) #V2 é uma cópia de full_dados, e a utilizo aqui para quantificar quantos alunos estão em um determinado grupamento de notas. Note que preciso v2 (que é um objeto) para uma lista, para que eu quantificar antes de exibir o len, de fato.
+    print(f"Quantidade de alunos do agrupamento {agrupamentos}:  {quantidade}")
+
