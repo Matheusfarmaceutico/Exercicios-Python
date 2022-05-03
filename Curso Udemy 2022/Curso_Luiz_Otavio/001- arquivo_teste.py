34d0e5f11b58b7bd7b8611a91174eeae479e0baa
Matheus Alves Resende Souza <matheus@pop-os.localdomain>
2022-05-03 17:46:35 -0300

    soma-listas-com-func

--- Curso Udemy 2022/Curso_Luiz_Otavio/001- arquivo_teste.py
+++ Curso Udemy 2022/Curso_Luiz_Otavio/001- arquivo_teste.py
@@ -4,7 +4,8 @@ lista1 = [1,2,3,4,5,6]
 lista2 = [6,7,8,9,10]
 
 
-def soma(*args):
-    lista_soma = [x + y for x,y in zip(args)]
+def soma(lista1,lista2):
+    lista_soma = [x + y for x,y in zip(lista1,lista2)]
+    return lista_soma
 
 print(soma(lista1,lista2))
