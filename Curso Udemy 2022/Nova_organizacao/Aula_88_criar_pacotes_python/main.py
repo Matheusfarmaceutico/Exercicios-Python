beafe894cc85254f44cd7c16659aaf08c18350a9
Matheus Alves <matheus_alvesrs13@hotmail.com>
2022-05-22 21:38:47 -0300

    aula88-finalizada

--- Curso Udemy 2022/Nova_organizacao/Aula_88_criar_pacotes_python/main.py
+++ Curso Udemy 2022/Nova_organizacao/Aula_88_criar_pacotes_python/main.py
@@ -1,7 +1,13 @@
-import vendas.calc_preco
 
-print(vendas.calc_preco.soma(2,2))
+from vendas.calc_preco import aumento,reducao
+
+preco = 49.90
+
+aumento_preco = aumento(valor=preco,porcentagem=15, formata=True)
+print(f"{aumento_preco}")
 
 
+preco_reducao = reducao(valor=preco,porcentagem=30, formata=True)
+print(f"{preco_reducao}")
 
 
