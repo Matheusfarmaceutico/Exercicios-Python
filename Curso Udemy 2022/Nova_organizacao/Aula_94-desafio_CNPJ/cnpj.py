f51c7b1b654333fadcdee429c7f68fb9bc1dfed0
Matheus Alves <matheus_alvesrs13@hotmail.com>
2022-06-07 00:33:03 -0300

    desafio-cnpj-andamento

--- Curso Udemy 2022/Nova_organizacao/Aula_94-desafio_CNPJ/cnpj.py
+++ Curso Udemy 2022/Nova_organizacao/Aula_94-desafio_CNPJ/cnpj.py
@@ -1,4 +1,5 @@
 
+from curses.ascii import isalnum
 import re
 "04.252.011/0001-10"
 "04252011000110"
@@ -8,17 +9,23 @@ def valida(argumento):
     tratado = elimina_caracter(argumento)
     sem_digitos = eliminar_digitos_padrao(tratado)
     converte_inteiro = converter_para_inteiro(sem_digitos)
-    teste = multiplicacao_e_soma(converte_inteiro)
-    print(teste)
+    soma_e_multiplicacao = multiplicacao_e_soma(converte_inteiro)
+    print(soma_e_multiplicacao)
+   
 
-
+def number_of_characters(opcao):
+    opcao = str(opcao)
+    if len(opcao) == 14 or len(opcao) == 18:
+        return True
+    else:
+        return False
+   
+        
 def elimina_caracter(cnpj):
-
     return re.sub(r"([^0-9])","",cnpj)
 
 
 def eliminar_digitos_padrao(cnpj_sem_caracter):
-
     cnpj_sem_d1_d2 = cnpj_sem_caracter[:-2]
     return cnpj_sem_d1_d2
 
@@ -35,3 +42,11 @@ def multiplicacao_e_soma(convertido_for_int):
     multi = [x * y for x,y in zip(juntar,convertido_for_int)]
     return sum(multi)
 
+
+while True:
+    user = input("Insira: ").strip()
+    if number_of_characters(user) == True:
+        valida(user)
+        break
+    else:
+        print("Número de caracteres inválido! Tente novamente.")
